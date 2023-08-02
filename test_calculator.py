c9e72d599cd55042ea1cceff3ff88c819c637adf
Vikhyat Gupta <vikhyat@suraasa.com>
2023-08-02 15:21:33 +0530

    feature: added new test cases for string calculator

--- test_calculator.py
+++ test_calculator.py
@@ -11,7 +11,14 @@ class TestStringCalculator(unittest.TestCase):
     This class tests all the possible scenarios for string calculator.
     """
 
-    raise NotImplementedError()
+    def test_empty_string(self):
+        self.assertEqual(string_calculator(number_string=""), 0)
+
+    def test_single_number_string(self):
+        self.assertEqual(string_calculator(number_string="1"), 1)
+
+    def test_two_number_string(self):
+        self.assertEqual(string_calculator(number_string="1,2"), 3)
 
 
 if __name__ == "__main__":
