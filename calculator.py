1ee43dcf325babfc8fb1939823b91f77ab117672
Vikhyat Gupta <vikhyat@suraasa.com>
2023-08-02 15:32:36 +0530

    refactor: handled number string with new line use case

--- calculator.py
+++ calculator.py
@@ -6,5 +6,11 @@ This file contains all the calculators.
 def string_calculator(number_string: str) -> int:
     if not number_string:
         return 0
-
-    return sum(map(lambda x: int(x), number_string.split(",")))
+    DEFAULT_DELIMITER = ","
+    extra_delimiters = ["\n"]
+    for delim in extra_delimiters:
+        number_string = number_string.replace(delim, DEFAULT_DELIMITER)
+    try:
+        return sum(map(lambda x: int(x), number_string.split(",")))
+    except ValueError:
+        raise Exception("Invalid number string")
