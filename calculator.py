7d7ae06cf18861c4ac53ca70d13178e1ef9becd1
Vikhyat Gupta <vikhyat@suraasa.com>
2023-08-02 18:31:01 +0530

    documentation: added file level docstring and necessary comments within code

--- calculator.py
+++ calculator.py
@@ -5,22 +5,29 @@ import re
 
 
 def string_calculator(number_string: str) -> int:
+    """
+    This function takes a string with numbers and delimiters
+    and returns the sum of all the numbers in the string
+    """
     if not number_string:
         return 0
+
     DEFAULT_DELIMITER = ","
     extra_delimiters = ["\n"]
 
     stripped_string = number_string.lstrip("/")
     if stripped_string != number_string:
-        # splitting on newline character since additional delimiters
-        # will be given in format "//[delimiter]\n"
+        # splitting on newline character since any additional delimiters
+        # will be given in format "//[delimiter1][delimiter2]\n"
         values = stripped_string.split("\n")
         delims = re.findall(r"\[(.*?)\]", values[0])
         extra_delimiters.extend(delims)
         number_string = "\n".join(values[1:])
 
+    # replacing every extra delimiter with the default delimiter
     for delim in extra_delimiters:
         number_string = number_string.replace(delim, DEFAULT_DELIMITER)
+
     try:
         numbers_list = list(map(lambda x: int(x), number_string.split(",")))
     except ValueError:
