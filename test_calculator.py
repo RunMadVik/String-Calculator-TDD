24313e45e9ab71375e44c95fe0909094c29c27a0
Vikhyat Gupta <vikhyat@suraasa.com>
2023-08-02 15:07:18 +0530

    feature: added test cases for string calculator with no internal implementation

---
+++ test_calculator.py
@@ -0,0 +1,18 @@
+"""
+This file contains all the test cases for calculators.
+"""
+
+import unittest
+from calculator import string_calculator
+
+
+class TestStringCalculator(unittest.TestCase):
+    """
+    This class tests all the possible scenarios for string calculator.
+    """
+
+    raise NotImplementedError()
+
+
+if __name__ == "__main__":
+    unittest.main()
