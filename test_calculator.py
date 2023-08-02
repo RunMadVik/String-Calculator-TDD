d2b4a99b5ed50e773295f5a1b8bf48ddd663ed19
Vikhyat Gupta <vikhyat@suraasa.com>
2023-08-02 15:47:31 +0530

    feature: added number string with different delimiter test case

--- test_calculator.py
+++ test_calculator.py
@@ -26,6 +26,9 @@ class TestStringCalculator(unittest.TestCase):
     def test_number_string_calculator_with_new_line_characters(self):
         self.assertEqual(string_calculator(number_string="1\n2,3"), 6)
 
+    def test_number_string_calculator_with_different_delimiter(self):
+        self.assertEqual(string_calculator(number_string="//;\n1;2"), 3)
+
 
 if __name__ == "__main__":
     unittest.main()
