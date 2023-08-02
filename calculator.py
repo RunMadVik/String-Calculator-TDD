1f4a5c3d3e2c81e77907f0f2eea5a03aaf0f4ef7
Vikhyat Gupta <vikhyat@suraasa.com>
2023-08-02 15:23:51 +0530

    refactor: handled one or two number strings use case

--- calculator.py
+++ calculator.py
@@ -6,3 +6,5 @@ This file contains all the calculators.
 def string_calculator(number_string: str) -> int:
     if not number_string:
         return 0
+
+    return sum(map(lambda x: int(x), number_string.split(",")))
